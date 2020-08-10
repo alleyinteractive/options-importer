abcb133a7e826cfb81df15230b22a3131128d372
Kyle Benk <kjbenk@gmail.com>
2020-08-10 09:12:04 -0600

    Ensure plugin is loaded during unit tests

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -17,29 +17,7 @@ require_once $_tests_dir . '/includes/functions.php';
  * Setup our environment (theme, plugins).
  */
 function _manually_load_environment() {
-	// Unhook actions that cause problems with running tests locally.
-	remove_action( 'switch_theme', 'rri_wpcom_action_switch_theme' );
-
-	/*
-	 * Tests won't start until the uploads directory is scanned, so use the
-	 * lightweight directory from the test install.
-	 *
-	 * @see https://core.trac.wordpress.org/changeset/29120.
-	 */
-	add_filter(
-		'pre_option_upload_path',
-		function () {
-			return ABSPATH . 'wp-content/uploads';
-		}
-	);
-
-	// Set up plugins.
-	update_option(
-		'active_plugins',
-		array(
-			'options-importer/options-importer.php',
-		)
-	);
+	require dirname( __FILE__ ) . '/../options-import.php';
 }
 tests_add_filter( 'muplugins_loaded', '_manually_load_environment' );
 
